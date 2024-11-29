ad0f0ed597692561a16f4b9f2b1b8b8e63f7a067
Henrique Cardoso de Faria <elo.henrique@gmail.com>
2024-11-29 14:59:06 +0000

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -455,6 +455,17 @@ locations = {
       link: 'https://balticruby.org/',
       img: 'https://thumb.tildacdn.com/tild6166-3665-4835-b736-363836373334/-/format/webp/logovector.png.webp'
     },
+    {
+      slug: 'euruko-2025',
+      name: 'Euruko 2025',
+      event_date: 'Viana do Castelo, Portugal',
+      location: 'September 18 - 19, 2025',
+      description: 'The largest and longest running annual Ruby conference in Europe, since 2003, that brings you all the good news about Ruby and its friends. Every year it takes place in a different European city, the location being chosen by participants.',
+      latitude: 41.6936837,
+      longitude: -8.8300416,
+      link: 'https://2025.euruko.org/',
+      img: './euruko-2025-logo.webp'
+    }
   ],
 }
 
